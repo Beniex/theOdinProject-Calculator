a37efd9514d662db36b6f1e32b5e61d23f5e544c
Benoit <benoit.exbrayat@gmail.com>
2024-05-08 14:42:25 +0200

    add the clear and the dot buttons
    add the clear button which deletes the current display
    add the dot button which had a dot in the current number and does not do anything if the number does
        alreday have a point into it
    change the GUI to fit the new buttons

--- script.js
+++ script.js
@@ -1,56 +1,7 @@
-function add(number1, number2){
-    return number1 + number2; 
-}
 
-function substract(number1, number2){
-    return number1 - number2; 
-}
-
-function multiply(number1, number2){
-    return number1*number2; 
-}
-
-function divide (number1, number2){
-    if (number2 == 0){
-        return 1234567890; 
-    } else {
-    return number1/number2; 
-    }
-}
-
-function operate(array){
-    let result; 
-    let number1 = array[0]; 
-    let number2 = array[1]; 
-    let operator = array[2]; 
-    switch(operator){
-        case "+": 
-            result = add(number1, number2); 
-            break; 
-        case "-": 
-            result = substract(number1, number2); 
-            break; 
-        case "*": 
-            result = multiply(number1, number2); 
-            break; 
-        case "/": 
-            result = divide(number1, number2); 
-            break;
-    }    
-    return result 
-}
 var displayContent=""; 
-/*var number1; 
-var number2; 
-var operator;
-*/ 
+
 let display = document.getElementById("display"); 
-
-function addSymbolToTheDisplay(keySymbol){
-    displayContent+= keySymbol; 
-    display.textContent = displayContent; 
-}
-
 
 
 const sevenKey = document.getElementById("sevenKey"); 
@@ -100,18 +51,12 @@ twoKey.addEventListener("click", () => {
 const threeKey = document.getElementById("threeKey"); 
 threeKey.addEventListener("click", () => {
     addSymbolToTheDisplay(3)}
-    );  
-const backKey = document.getElementById("backKey"); 
-backKey.addEventListener("click", () => {
-    console.log("avant back");
-    console.log(typeof displayContent);
-    let str = displayContent.toString().split('').slice(0, -1);
-    displayContent = str;
-    displayContent =str.join('');
-    display.textContent = displayContent;
-    console.log("après back");
-    console.log(typeof displayContent);
-    });
+    ); 
+const clearKey = document.getElementById("clearKey"); 
+clearKey.addEventListener("click", () => {
+    displayContent =""; 
+    display.textContent = displayContent; 
+});
 
 const substractKey = document.getElementById("substractKey"); 
 substractKey.addEventListener("click", () => {
@@ -125,8 +70,62 @@ const addKey = document.getElementById("addKey");
 addKey.addEventListener("click", () => {
     addSymbolToTheDisplay("+")}
     ); 
+const backKey = document.getElementById("backKey"); 
+backKey.addEventListener("click", () => {
+    let str = displayContent.split('').slice(0, -1);
+    displayContent =str.join('');
+    display.textContent = displayContent;
+    });
 
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7","8","9"]; 
+const equalKey = document.getElementById("equalKey"); 
+equalKey.addEventListener("click", () => { 
+    refreshDisplay(); 
+}); 
+const dotKey = document.getElementById("dotKey"); 
+dotKey.addEventListener("click", () => {
+    let number1 = retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()[0]; 
+    let number2 = retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()[1];
+    if(isTypingNumber1() && !hasAPoint(number1)){
+        addSymbolToTheDisplay("."); 
+    }
+    if(isTypingNumber2() && !hasAPoint(number2)){
+        addSymbolToTheDisplay("."); 
+    }
+});
+
+function addSymbolToTheDisplay(keySymbol){
+    displayContent+= keySymbol; 
+    display.textContent = displayContent; 
+}
+
+function isTypingNumber1(){
+    let number1 = retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()[0]; 
+    let number2 = retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()[1]; 
+    if (number1 !== undefined && number2 == undefined){
+        return true; 
+    } else {
+        return false; 
+    }
+}
+function isTypingNumber2(){
+    let number1 = retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()[0]; 
+    let number2 = retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()[1];
+    if (number1 !== undefined && number2 !== undefined){
+        return true; 
+    } else {
+        return false; 
+    }
+}
+function hasAPoint(number){
+    let numberArray = String(number).split('');
+    if(numberArray.includes(".")){
+        return true 
+    } else {
+        return false; 
+    }
+}
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7","8","9","."]; 
 function isANumber(character){
     if (numbers.indexOf(character)!== -1){
         return true; 
@@ -189,15 +188,17 @@ function retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay(){
         if (isANumber(analyzedChar)){
             hubString += analyzedChar; 
             if(isTheLastCharacter(displayContentArray, analyzedChar) && areStocked(number1, operator)){
-                number2 = parseInt(hubString); 
-            } 
+                number2 = parseFloat(hubString); 
+            } else if ( isTheLastCharacter(displayContentArray, analyzedChar)){
+                number1 = parseFloat(hubString);
+            }
         } else if (isAnOperator(analyzedChar)){
             if (areStocked(number1, operator)){
-                number2 = parseInt(hubString);
+                number2 = parseFloat(hubString);
                 break;                 
             } else {
             operator = analyzedChar;
-            number1 = parseInt(hubString); 
+            number1 = parseFloat(hubString); 
             hubString = ""; 
             }
         } 
@@ -217,24 +218,59 @@ function refreshDisplay(){
 function showResult(){
     let remainOfTheDisplayContent = retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()[3]; 
     let resultOfTheCalcul = operate(retrieveNumber1and2AndOperatorAndRemainingDisplayFromDisplay()); 
-    /*display.textContent= resultOfTheCalcul;*/
     if (hasELement(remainOfTheDisplayContent)){
     displayContent = resultOfTheCalcul+ remainOfTheDisplayContent.join('');
     displayContent = displayContent.toString();
     display.textContent= displayContent; 
 
     } else {
-    displayContent = resultOfTheCalcul;
-    displayContent = displayContent.toString();
+    displayContent = resultOfTheCalcul.toString();
     display.textContent= displayContent;
     }
 }
 
+function add(number1, number2){
+    return number1 + number2; 
+}
+
+function substract(number1, number2){
+    return number1 - number2; 
+}
+
+function multiply(number1, number2){
+    return number1*number2; 
+}
+
+function divide (number1, number2){
+    if (number2 == 0){
+        return 1234567890; 
+    } else {
+    return number1/number2; 
+    }
+}
+
+function operate(array){
+    let result; 
+    let number1 = array[0]; 
+    let number2 = array[1]; 
+    let operator = array[2]; 
+    switch(operator){
+        case "+": 
+            result = add(number1, number2); 
+            break; 
+        case "-": 
+            result = substract(number1, number2); 
+            break; 
+        case "*": 
+            result = multiply(number1, number2); 
+            break; 
+        case "/": 
+            result = divide(number1, number2); 
+            break;
+    }    
+    return result 
+}
 
 
-const equalKey = document.getElementById("equalKey"); 
-equalKey.addEventListener("click", () => { 
-    refreshDisplay(); 
-}); 
 
 
